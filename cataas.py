42b490a20b7b8834a25d401bb05c3f3105dd8282
irinaumova01 <irinaumova01@gmail.com>
2024-11-23 11:08:38 +0500

    создано окно и виджеты

--- cataas.py
+++ cataas.py
@@ -1,1 +1,22 @@
-from
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO #помогает работать с байтами из интернета
+
+from pygame.examples.moveit import load_image
+
+window=Tk()
+window.title("cats")
+window.geometry("600x480")
+
+label=Label()
+label.pack()
+
+url="httds://cataas.com/cat"
+img=load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image=img
+
+window.mainloop()
